f42d5cda0e1fc1ba7cf462456694997afac9a633
Yang Jie Lin Hu <yl14925@ic.ac.uk>
2025-11-23 18:48:39 +0000

    Created basic Flask App implementation

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import request, url_for
+from flask import Flask, request, render_template
 
 from sqlalchemy import create_engine, select, delete
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import Session
 
 from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def page_home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        debug=True
+    )
