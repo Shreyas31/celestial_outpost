2c71ea8bb801b5fe0ec46f527be307e442b90e85
Yang Jie Lin Hu <yl14925@ic.ac.uk>
2025-11-25 23:36:29 +0000

    Changed way to run app to 'flask run'

--- app.py
+++ app.py
@@ -12,7 +12,3 @@ app = Flask(__name__)
 @app.route("/")
 def page_home():
     return render_template("home.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
