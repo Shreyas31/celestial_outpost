46c8af2d7f318c92a411b9a9f93510c1c508abe5
Yang Jie Lin Hu <yl14925@ic.ac.uk>
2025-12-03 21:15:08 +0100

    Added method to add new star to db + extracted simbad queries to new file

---
+++ simbad_queries.py
@@ -0,0 +1,89 @@
+import csv
+from typing import Optional
+
+from astroquery.simbad import Simbad
+
+
+### OBJECT TYPES
+otype_to_label: dict[str, str] = {}
+label_to_desc: dict[str, str] = {}
+
+with open("object_types.csv", "r") as f:
+    f.readline()  # Remove headers
+
+    reader = csv.reader(f)
+
+    for line in reader:
+        key, label, desc = line
+        otype_to_label[key] = label
+        label_to_desc[label] = desc
+
+
+### SIMBAD QUERIES
+def query_star_name(common_name: str) -> Optional[str]:
+    """
+    Given the common name of a star, queries via SIMBAD to find
+    its scientific name. If the query does not match any object,
+    will return None.
+    """
+    simbad = Simbad()
+    q = simbad.query_object(common_name)
+
+    if not q:
+        # Query did not return find any star
+        return None
+
+    return q["main_id"][0]
+
+
+def query_star_details(object_name: str) -> dict[str, str]:
+    """
+    Given the name of an object, returns its details as a dict of
+    strings. Currently, this includes:
+      - `main_id`: name (designation).
+      - `ra`: Right Ascension.
+      - `dec`: Declination.
+      - `sp_type`: Spectral type (color).
+      - `otype`: Object type.
+      - `app_mag`: Apparent magnitude of star at measured filter.
+      - `filter`: Filter used for apparent magnitude measurement.
+
+    If the query does not match any SIMBAD object, throws a ValueError.
+    query_star_name() can be used first to check whether the object exsits.
+    """
+    simbad = Simbad()
+    simbad.add_votable_fields("sp_type", "allfluxes", "otype")
+    q = simbad.query_object(object_name)
+
+    star_data: dict[str, str] = {}
+
+    if not q:
+        raise ValueError(f"{object_name} does not match any query on Simbad.")
+
+    # Add the simple headers:
+    simple_headers = ("main_id", "ra", "dec", "sp_type")
+    for head in simple_headers:
+        star_data[head] = q[head][0]
+
+    # Add object type:
+    star_data["otype"] = otype_to_label[q["otype"][0]]
+
+    # Query for fluxes:
+    star_data["app_mag"] = ""
+    star_data["filter"] = ""
+
+    # Order in which to check for existing magnitude measurements.
+    for filter in ("V", "G", "g", "r", "B", "R", "i", "I"):
+        if q[filter][0]:
+            star_data["app_mag"] = q[filter][0]
+            star_data["filter"] = filter
+            break
+
+    return star_data
+
+
+def get_full_type_description(object_type: Optional[str]) -> Optional[str]:
+    if object_type is None:
+        return None
+
+    return label_to_desc.get(object_type)
--- star.py
+++ star.py
@@ -1,12 +1,18 @@
-from flask import Blueprint, request, render_template, redirect, url_for
-from astroquery.simbad import Simbad
-from typing import Optional
-from models.star import Star
-
 import os
+from typing import Optional
+
+from flask import Blueprint, request, render_template, redirect, url_for
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, select
+
+from models.star import Star
+from simbad_queries import (
+    query_star_name,
+    query_star_details,
+    get_full_type_description,
+)
+
 
 star_bp = Blueprint("star", __name__, url_prefix="/star")
 
@@ -21,8 +27,6 @@ url = URL.create(
 )
 
 
-print(url)
-
 engine = create_engine(url)
 
 
@@ -31,32 +35,23 @@ def home():
     return render_template("star/home.html")
 
 
-def query_star_name(common_name: str) -> Optional[str]:
-    """
-    Given the common name of a star, queries via SIMBAD
-    to find its scientific name (stored in the db).
-    """
-    simbad = Simbad()
-    q = simbad.query_object(common_name)
-
-    if not q:
-        # Query did not return find any star
-        return
-
-    return q["main_id"][0]
-
-
 @star_bp.route("/search", methods=["POST"])
 def query_star():
-    common_name = request.form["starname"]
+    queried_name = request.form.get("f_starname", default="")
 
-    starname = query_star_name(common_name)
+    starname = query_star_name(queried_name)
 
     if not starname:
         # TODO: Add "error message" to user for "star not found"
         return render_template("star/home.html")
 
-    return redirect(url_for("star.detail_star", starname=starname))
+    return redirect(
+        url_for(
+            "star.detail_star",
+            starname=starname,
+            queried_name=queried_name,
+        )
+    )
 
 
 @star_bp.route("/<starname>")
@@ -66,10 +61,54 @@ def detail_star(starname: str):
         star = session.execute(stmt).scalar_one_or_none()
 
         observations = star.observations if star else []
+        startype = get_full_type_description(star.startype) if star else None
+
+    queried_name = request.args.get("queried_name")
 
     return render_template(
         "star/detail.html",
         starname=starname,
+        queried_name=queried_name,
         star=star,
+        startype=startype,
         observations=observations,
     )
+
+
+def find_existing_or_create_star(common_name: str) -> Optional[int]:
+    """
+    Given the common name of a star, will:
+      - If the star's name does not exist, will return None.
+      - If the star already exists in the db, will return the star's ID.
+      - If the star doesn't exist in the db, will create it, and
+        return the star's ID.
+
+    This should be used by observation.py whenever a new observation
+    is created for a star that does not exist, or to bind it to an
+    existing star.
+    """
+    starname: Optional[str] = query_star_name(common_name)
+
+    # Star's name cannot be found
+    if not starname:
+        return None
+
+    with Session(engine) as session:
+        stmt = select(Star).where(Star.starname == starname)
+        star = session.execute(stmt).scalar_one_or_none()
+
+        # Create star if it does not exist:
+        if not star:
+            details = query_star_details(starname)
+
+            star = Star(
+                starname=starname,
+                startype=details["otype"],
+                coordra=details["ra"],
+                coorddec=details["dec"],
+                color=details["sp_type"],
+                appmagnitude=details["app_mag"],
+                measurefilter=details["filter"],
+            )
+
+    return star.id
