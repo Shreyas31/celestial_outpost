43f81470eb9ba56c0f80dc1103a2c658b3a23ced
Yang Jie Lin Hu <yl14925@ic.ac.uk>
2025-12-07 21:41:04 +0000

    Updated simbad query to allow for common name

--- helpers/simbad_queries.py
+++ helpers/simbad_queries.py
@@ -26,24 +26,31 @@ with open("helpers/object_types.csv", "r") as f:
 def query_star_name(common_name: str) -> Optional[str]:
     """
     Given the common name of a star, queries via SIMBAD to find
-    its scientific name. If the query does not match any object,
-    will return None.
+    its name. If the query does not match any object, will return None.
     """
     simbad = Simbad()
+    simbad.add_votable_fields("ids")
     q = simbad.query_object(common_name)
 
     if not q:
         # Query did not return find any star
         return None
 
-    return q["main_id"][0]
+    # Get object name. If it has a common name, use common name.
+    # Otherwise, use main id (designation).
+    name: str = q["main_id"][0]
+    for val in q["ids"][0].split("|"):
+        if len(val) > 4 and val[:4] == "NAME":
+            name = val[5:]
+
+    return name
 
 
 def query_star_details(object_name: str) -> dict[str, Any]:
     """
     Given the name of an object, returns its details as a dict of
     strings. Currently, this includes:
-      - `main_id`: name (designation).
+      - `main_id`: Name. Common name if possible; otherwise, designation.
       - `ra`: Right Ascension.
       - `dec`: Declination.
       - `sp_type`: Spectral type (color).
@@ -55,7 +62,7 @@ def query_star_details(object_name: str) -> dict[str, Any]:
     query_star_name() can be used first to check whether the object exsits.
     """
     simbad = Simbad()
-    simbad.add_votable_fields("sp_type", "allfluxes", "otype")
+    simbad.add_votable_fields("sp_type", "allfluxes", "otype", "ids")
     q = simbad.query_object(object_name)
 
     star_data: dict[str, Any] = {}
@@ -64,9 +71,16 @@ def query_star_details(object_name: str) -> dict[str, Any]:
         raise ValueError(f"{object_name} does not match any query on Simbad.")
 
     # Add the simple headers:
-    simple_headers = ("main_id", "ra", "dec", "sp_type")
+    simple_headers = ("ra", "dec", "sp_type")
     for head in simple_headers:
         star_data[head] = q[head][0]
+
+    # Get object name. If it has a common name, use common name.
+    # Otherwise, use main id (designation).
+    star_data["name"] = q["main_id"][0]
+    for val in q["ids"][0].split("|"):
+        if len(val) > 4 and val[:4] == "NAME":
+            star_data["name"] = val[5:]
 
     # Add object type:
     star_data["otype"] = otype_to_label.get(q["otype"][0], "Unknown")
